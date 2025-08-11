471f8a9085d231d5b7e67247ffaf1e7622c1db66
brad-basilio <brad.basilioppy@gmail.com>
2025-08-11 08:51:24 -0500

    cast testimony

--- app/Models/Testimony.php
+++ app/Models/Testimony.php
@@ -24,6 +24,11 @@ class Testimony extends Model
         'case'
 
     ];
+    protected $casts = [
+        'visible' => 'boolean',
+        'status' => 'boolean',
+
+    ];
     public function lang()
     {
         return $this->belongsTo(Lang::class);
